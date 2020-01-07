7adf18041cad488941ad63a2cc4b126aee05df25
github@cybertekschool.com <github@cybertekschool.com>
2020-01-07 11:53:35 -0500

    Karate Demo for API Testing

--- src/test/java/examples/users/UsersRunner.java
+++ src/test/java/examples/users/UsersRunner.java
@@ -7,6 +7,11 @@ class UsersRunner {
     @Karate.Test
     Karate testUsers() {
         return new Karate().feature("users").relativeTo(getClass());
-    }    
+    }
+
+    @Karate.Test
+    Karate testZipCode() {
+        return new Karate().feature("ZipCode").relativeTo(getClass());
+    }
 
 }
